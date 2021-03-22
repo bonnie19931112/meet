5901942cab304e3cf7058cd7df1877d500595139
bonnie19931112 <bonnie111282@gmail.com>
2021-03-22 19:50:24 +0800

    添加導覽列

--- main.js
+++ main.js
@@ -3,7 +3,34 @@ $(".carousel").carousel({
 })
 
 
+$("#works").slick({
+    slidesToShow:3,
+    slidesToScroll:2,
 
+    responsive: [
+         // tablet
+        // under 1025 size what will do is...?
+        {
+            breakpoint: 1025,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+
+        // phone
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+
+    ]
+
+
+});
 
 
 // AOS.init({
